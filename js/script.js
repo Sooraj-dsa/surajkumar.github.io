cba4a191e6bcf5f69c70c23abefa88eed9f79891
Suraj Kumar Gupta <134607439+Sooraj-dsa@users.noreply.github.com>
2023-10-24 23:01:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 /*================================= typing animation ===============================*/
 var typed = new Typed(".typing",{
-    strings:[" ","Data Scientist","Machine Learning Engineer","Deep Learning Engineer","Web Developer"],
+    strings:[" ","Data Analyst","Data Science","Machine Learning","PowerBI Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
 })
 /*
 var typed = new Typed(".text",{
-    strings:[" ","I'm a data scientist with extensive internship experience forover 2 years. My expertise is to cleaning data and crating a good model for best data prediction. I you want to say,- who i am ?.. you can say Machine Learning Engineer,Computer Vision,Natural Language Processing and many more....."],
+    strings:[" ","I'm a data Analyst with extensive internship experience forover 2 years. My expertise is to cleaning data and crating a good model for best data prediction. I you want to say,- who i am ?.. you can say Machine Learning Engineer,Computer Vision,Natural Language Processing and many more....."],
     typeSpeed:10,
     BackSpeed:10,
 })*/
 
 /*
 var typed = new Typed(".name",{
-    strings:[" ","Dipendra Pratap Singh","and Nick is Dipak","Dreg_Warrior","Alone Walker","Leader","Dipendra Pratap"],
+    strings:[" ","Suraj Kumar Gupta","and Jack is Suraj","Dreg_Warrior","Alone Walker","Leader","Suraj Gupta"],
     typeSpeed:100,
     BackSpeed:60,
     
 })*/
 
 var typed = new Typed(".name",{
-    strings:[" ","Dip","Dipendra","Dipendra Pratap","Dipendra Pratap Singh"],
+    strings:[" ","Sur","Suraj","Suraj Kumar","Suraj Kumar Gupta"],
     typeSpeed:100,
     BackSpeed:60,
     
